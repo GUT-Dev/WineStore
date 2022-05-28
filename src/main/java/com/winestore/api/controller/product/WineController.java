9f6664a0849b5112bdf8c99aa06d2760d75461ec
Vitalij <r.vitalik.skuratov@gmail.com>
2022-05-29 02:38:39 +0300

    Refactoring

--- src/main/java/com/winestore/api/controller/product/WineController.java
+++ src/main/java/com/winestore/api/controller/product/WineController.java
@@ -3,15 +3,11 @@ package com.winestore.api.controller.product;
 import com.winestore.api.dto.filters.WineSearchFilter;
 import com.winestore.api.dto.product.WineDTO;
 import com.winestore.api.dto.product.WineListDTO;
-import com.winestore.api.mapper.product.BrandMapper;
-import com.winestore.api.mapper.product.LandMapper;
 import com.winestore.api.mapper.product.WineMapper;
 import com.winestore.domain.entity.product.Wine;
 import com.winestore.enums.AvailableStatus;
 import com.winestore.enums.Sweetness;
 import com.winestore.enums.WineType;
-import com.winestore.service.product.BrandService;
-import com.winestore.service.product.LandService;
 import com.winestore.service.product.WineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,30 +22,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WineController {
 
-    private final WineService wineService;
-    private final BrandService brandService;
-    private final LandService landService;
-    private final LandMapper landMapper;
-    private final BrandMapper brandMapper;
-    private final WineMapper wineMapper;
+    private final WineService service;
+    private final WineMapper mapper;
 
     @GetMapping("/{id}")
     public WineDTO getWine(@PathVariable(name = "id") Long id) {
-        WineDTO dto = wineMapper.toDTO(wineService.getById(id));
-        dto.setRating(wineService.countRating(dto.getId()));
+        WineDTO dto = mapper.toDTO(service.getById(id));
+        dto.setRating(service.countRating(dto.getId()));
         return dto;
     }
 
     @GetMapping
     public Page<WineListDTO> getPage(WineSearchFilter filter, Pageable pageable) {
-        return wineService.getPage(filter, pageable)
-            .map(wineMapper::toListDTO);
+        return service.getPage(filter, pageable)
+            .map(mapper::toListDTO);
     }
 
     @PostMapping
     public WineDTO create(@RequestBody WineDTO dto) {
-        Wine wine = wineService.create(wineMapper.toEntity(dto));
-        return wineMapper.toDTO(wine);
+        Wine wine = service.create(mapper.toEntity(dto));
+        return mapper.toDTO(wine);
     }
 
     @GetMapping("/sweetness")
@@ -68,7 +60,7 @@ public class WineController {
 
     @GetMapping("/max-price")
     public String getMaxPrice() {
-        return wineService.getMaxPrice()
+        return service.getMaxPrice()
             .movePointLeft(2)
             .setScale(0)
             .toString();
@@ -76,7 +68,7 @@ public class WineController {
 
     @GetMapping("/min-price")
     public String getMinPrice() {
-        return wineService.getMinPrice()
+        return service.getMinPrice()
             .movePointLeft(2)
             .setScale(0)
             .toString();
